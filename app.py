216afe4077cdc152686b36cbc8d49903a251141b
jonathan10620 <jonathan10620@gmail.com>
2021-04-12 16:26:27 -0500

    added database helpers

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, g, request
+import sqlite3
+
+app = Flask(__name__)
+
+
+def connect_db():
+    sql = sqlite3.connect(
+        "/Users/jonathandelapaz/oreilly/flask/projects/food_tracker_app/food_log.db"
+    )
+    sql.row_factory = sqlite3.Row
+    return sql
+
+
+def get_db():
+    if not hasattr(g, "sqlite3_db"):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, "sqlite_db"):
+        g.sqlite_db.close()
+
+
+@app.route("/")
+def index():
+    return render_template("home.html")
+
+
+@app.route("/view")
+def view():
+    return render_template("day.html")
+
+
+@app.route("/food", methods=["GET", "POST"])
+def food():
+    if request.method == "POST":
+        name = request.form["food-name"]
+        protein = request.form["protein"]
+        carb = request.form["carb"]
+        fat = request.form["fat"]
+
+        return f"{name}, {protein}, {carb},{fat}"
+
+    return render_template("add_food.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
